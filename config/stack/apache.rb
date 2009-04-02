3067728c99be4c9884a37ef2c8af87b8f36fb9e2
Adam Meehan <adam.meehan@gmail.com>
2009-04-02 11:15:26 +0800

    update to passenger 2.1.3 and use a package version
    Signed-off-by: Ben Schwarz <ben.schwarz@gmail.com>

--- config/stack/apache.rb
+++ config/stack/apache.rb
@@ -18,6 +18,7 @@ end
 
 package :passenger, :provides => :appserver do
   description 'Phusion Passenger (mod_rails)'
+  version '2.1.3'
   gem 'passenger' do
     post :install, 'echo -en "\n\n\n\n" | sudo passenger-install-apache2-module'
 
@@ -26,8 +27,8 @@ package :passenger, :provides => :appserver do
     post :install, 'touch /etc/apache2/extras/passenger.conf'
     post :install, 'echo "Include /etc/apache2/extras/passenger.conf"|sudo tee -a /etc/apache2/apache2.conf'
 
-    [%q(LoadModule passenger_module /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-2.1.2/ext/apache2/mod_passenger.so),
-    %q(PassengerRoot /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-2.1.2),
+    [%Q(LoadModule passenger_module /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}/ext/apache2/mod_passenger.so),
+    %Q(PassengerRoot /usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}),
     %q(PassengerRuby /usr/local/bin/ruby),
     %q(RailsEnv production)].each do |line|
       post :install, "echo '#{line}' |sudo tee -a /etc/apache2/extras/passenger.conf"
@@ -38,9 +39,9 @@ package :passenger, :provides => :appserver do
   end
 
   verify do
-    has_file '/etc/apache2/extras/passenger.conf'
-    has_file '/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-2.1.2/ext/apache2/mod_passenger.so'
-    has_directory '/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-2.1.2'
+    has_file "/etc/apache2/extras/passenger.conf"
+    has_file "/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}/ext/apache2/mod_passenger.so"
+    has_directory "/usr/local/ruby-enterprise/lib/ruby/gems/1.8/gems/passenger-#{version}"
   end
 
   requires :apache, :apache2_prefork_dev, :ruby_enterprise
