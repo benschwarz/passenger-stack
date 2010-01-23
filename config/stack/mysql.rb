1f488df81c98c20852140bc34fb75d0eda1ae0bb
James Chen <ashchan@gmail.com>
2010-01-23 11:14:30 +0800

    libmysqlclient-dev on ubuntu 9.10

--- config/stack/mysql.rb
+++ config/stack/mysql.rb
@@ -1,6 +1,6 @@
 package :mysql, :provides => :database do
   description 'MySQL Database'
-  apt %w( mysql-server mysql-client libmysqlclient15-dev )
+  apt %w( mysql-server mysql-client libmysqlclient-dev )
   
   verify do
     has_executable 'mysql'
